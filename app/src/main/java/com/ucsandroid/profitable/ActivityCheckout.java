f2506ba0881276a808b444ae3d7d326cd9058c05
deamon <deamondm@gmail.com>
2016-05-25 07:26:37 -0700

    Cleaning up NFC receipt

--- app/src/main/java/com/ucsandroid/profitable/ActivityCheckout.java
+++ app/src/main/java/com/ucsandroid/profitable/ActivityCheckout.java
@@ -10,10 +10,16 @@ import android.nfc.NfcEvent;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.MimeTypeMap;
 import android.widget.Toast;
 
 import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
+import static android.nfc.NdefRecord.TNF_WELL_KNOWN;
+import static android.nfc.NdefRecord.createExternal;
 import static android.nfc.NdefRecord.createMime;
 
 @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
@@ -48,40 +54,32 @@ public class ActivityCheckout extends AppCompatActivity implements NfcAdapter.Cr
     @Override
     public NdefMessage createNdefMessage(NfcEvent event) {
 
-        String text = ("Beam me up, Android!\n\n" +
-                "Beam Time: " + System.currentTimeMillis());
-
-
-        /*
-        NdefMessage msg = new NdefMessage(
-                NdefRecord.createExternal("example.com", "mycustomtype", payload),
-                NdefRecord.createApplicationRecord("com.example.your.app.package")
-        )*/
 
         String name = "yoyo";
         String title = "brobro";
 
 
-        //Bitmap mBitmap = mPhoto;
-        //ByteArrayOutputStream stream = new ByteArrayOutputStream();
-        //mBitmap.compress(Bitmap.CompressFormat.JPEG, 80, stream);
-        //byte[] byteArray = stream.toByteArray();
 
-        //NdefRecord picRecord = new NdefRecord(NdefRecord.TNF_MIME_MEDIA,  "image/jpeg".getBytes(), null, byteArray);
+        List<String> tesList = new ArrayList();
+        for(int a = 0; a < 10; a++){
+            tesList.add("num "+a);
+        }
+
         String informations = "name: "+name + "\ntitle: " + title + "\naddress: "+ name + "\ncity: "+ title + "\nphone: "+ name + "\nmail: "+title + "\n";
-        NdefRecord textRecord = createTextRecord(informations);
-        NdefMessage message = new NdefMessage(new NdefRecord[]{ textRecord}); //picRecord,
 
+        NdefRecord test = createMime(informations, informations.getBytes());
 
-        Uri myUri = Uri.parse("http://www.google.com");
-        NdefRecord rtdUriRecord2 = NdefRecord.createUri(myUri);
 
         NdefMessage msg = new NdefMessage(
                 new NdefRecord[] {
-                        rtdUriRecord2,
+                        test,
+                        test,
+                        test,
+                        test,
+                        test
+                        //rtdUriRecord2,
                         //createMime("application/com.sos.anchor", text.getBytes())
-                        createMime("image/jpeg", text.getBytes()),
-                        createMime(text, text.getBytes())
+                        //createMime(text, text.getBytes())
                         /**
                          * The Android Application Record (AAR) is commented out. When a device
                          * receives a push with an AAR in it, the application specified in the AAR
@@ -90,15 +88,32 @@ public class ActivityCheckout extends AppCompatActivity implements NfcAdapter.Cr
                          * activity starts when receiving a beamed message. For now, this code
                          * uses the tag dispatch system.
                         */
-                        //,NdefRecord.createApplicationRecord("com.example.android.beam")
+                        ,NdefRecord.createApplicationRecord("com.example.android.beam")
                 });
 
-        return message;
+        return msg;
+    }
+
+    private NdefRecord createUriRecord(String uri){
+
+        Uri myUri = Uri.parse("http://www.google.com");
+        return NdefRecord.createUri(myUri);
+
     }
 
     private NdefRecord createTextRecord(String informations) {
         return createMime(informations, informations.getBytes());
     }
+
+    private NdefRecord createBitampRecord(Bitmap bitmap){
+
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        byte[] byteArray = stream.toByteArray();
+
+        return new NdefRecord(NdefRecord.TNF_MIME_MEDIA,  "image/jpeg".getBytes(), null, byteArray);
+    }
+
 
 
 }
