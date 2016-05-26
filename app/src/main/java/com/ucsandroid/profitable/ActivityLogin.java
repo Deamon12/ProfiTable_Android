76141249a696aa67b7eb119f73e677cdd3357b0b
deamon <deamondm@gmail.com>
2016-05-25 23:27:40 -0700

    Starting LoginActivity volleys

--- app/src/main/java/com/ucsandroid/profitable/ActivityLogin.java
+++ app/src/main/java/com/ucsandroid/profitable/ActivityLogin.java
@@ -1,13 +1,23 @@
 package com.ucsandroid.profitable;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.view.View;
+import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.rey.material.widget.EditText;
-import com.rey.material.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class ActivityLogin extends AppCompatActivity implements View.OnClickListener {
 
@@ -20,6 +30,10 @@ public class ActivityLogin extends AppCompatActivity implements View.OnClickList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        if (!Singleton.hasBeenInitialized()) {
+            Singleton.initialize(this);
+        }
+
         loginButton = (CardView) findViewById(R.id.login_card);
         loginButton.setOnClickListener(this);
 
@@ -31,7 +45,29 @@ public class ActivityLogin extends AppCompatActivity implements View.OnClickList
         forgotButton = (TextView) findViewById(R.id.forgot_login);
         forgotButton.setOnClickListener(this);
 
+        checkUserLoggedIn();
 
+    }
+
+    private void checkUserLoggedIn() {
+
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(ActivityLogin.this);
+        if(settings.contains(getResources().getString(R.string.user_name)) &&
+                settings.contains(getResources().getString(R.string.rest_id)) &&
+                settings.contains(getResources().getString(R.string.user_pin))){
+            //User has logged in before
+            doLogin();
+        }
+    }
+
+    private void saveLoginInfo(String username, String restId, String pin){
+
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(ActivityLogin.this);
+        SharedPreferences.Editor edit = settings.edit();
+        edit.putString(getString(R.string.user_name), username);
+        edit.putString(getString(R.string.rest_id), restId);
+        edit.putString(getString(R.string.user_pin), pin);
+        edit.apply();
     }
 
     //Function to jump to the tableViewActivity page after log-in is succcessful
@@ -54,37 +90,103 @@ public class ActivityLogin extends AppCompatActivity implements View.OnClickList
 
         if(v == loginButton){
 
+            boolean doLogin = true;
             //Check editFields for errors - refactor perhaps
-            if(usernameField.getText().toString().equalsIgnoreCase("")){
+            if(usernameField.getText().toString().equalsIgnoreCase("")){    //Check username field
+                doLogin = false;
                 usernameField.setError("Username is required");
             }
             else{
                 usernameField.clearError();
             }
 
-            if(restaurantField.getText().toString().equalsIgnoreCase("")){
+            if(restaurantField.getText().toString().equalsIgnoreCase("")){  //Check restaurant field
+                doLogin = false;
                 restaurantField.setError("Restaurant Id is required");
             }
             else{
                 restaurantField.clearError();
             }
 
-            if(pinField.getText().toString().equalsIgnoreCase("")){
+            if(pinField.getText().toString().equalsIgnoreCase("")){         //Check pin field
+                doLogin = false;
                 pinField.setError("Pin is required");
             }
             else{
                 pinField.clearError();
             }
 
+            if(doLogin){            //Continue to volley if nothing errors out
+                doVolleyLogin(usernameField.getText().toString(), restaurantField.getText().toString(), pinField.getText().toString());
+            }
+
         }
         else if(v == forgotButton){
-
+            //todo
         }
-
-
 
     }
 
+    private void doVolleyLogin(String username, String restId, String pin){
+
+        //http://52.38.148.241:8080/com.ucsandroid.profitable/rest/employee/login?rest_id=1&account_name=bigX&account_pass=password
+        Uri.Builder builder = Uri.parse("http://52.38.148.241:8080").buildUpon();
+        builder.appendPath("com.ucsandroid.profitable")
+                .appendPath("rest")
+                .appendPath("employee")
+                .appendPath("login")
+                .appendQueryParameter("rest_id", restId)
+                .appendQueryParameter("account_name", username)
+                .appendQueryParameter("account_pass", pin);
+
+        String myUrl = builder.build().toString();
+
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest(Request.Method.GET,
+                myUrl,
+                (JSONObject) null,
+                successListener, errorListener);
+
+        // Access the RequestQueue through your singleton class.
+        Singleton.getInstance().addToRequestQueue(jsObjRequest);
+
+    }
+
+
+    private Response.Listener successListener = new Response.Listener() {
+        @Override
+        public void onResponse(Object response) {
+
+            try {
+                JSONObject theResponse = new JSONObject(response.toString());
+
+                if(theResponse.getBoolean("success") && theResponse.has("result")){
+
+
+                    String newData = theResponse.getJSONArray("result").toString();
+
+                    System.out.println("newData: "+newData);
+
+
+                    //saveLoginInfo();
+
+                }
+                else{
+                    //TODO:Results Error ((ActivityOrderView)getActivity()).showErrorSnackbar(theResponse.getString("message"));
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+    };
+
+    Response.ErrorListener errorListener = new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            System.out.println("Volley error: " + error);///TODO Connect/server error
+        }
+    };
 
 
 }
